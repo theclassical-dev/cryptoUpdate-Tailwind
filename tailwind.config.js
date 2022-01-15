21e816df2092883a2e4fb00e89e7c6d6b261c413
Abraham Peter <theclassical001@gmail.com>
2022-01-15 19:30:01 +0100

    initial commit + colors extended

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "c-black": "#000000",
+        "c-white": "#f7f7f7",
+        "c-red": "#FA5959",
+        "c-blue": "#243A45",
+        "c-gray": "#9194A2",
+      }
+    },
+  },
+  plugins: [],
+}
